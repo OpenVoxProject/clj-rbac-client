4865cd8284f219b254405ade82f745928acda3a7
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 22:52:31 +0000

    Version 1.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/rbac-client "1.1.8-SNAPSHOT"
+(defproject org.openvoxproject/rbac-client "1.1.8"
   :description "Tools for interacting with PE RBAC"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
