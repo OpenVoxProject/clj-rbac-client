7f348dd217fabfe7746918690755933e752dc935
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2026-05-09 22:52:03 +0000

    Version 1.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def i18n-version "1.0.4")
 (def jackson-version "2.21.3")
 
-(defproject org.openvoxproject/rbac-client "1.3.0"
+(defproject org.openvoxproject/rbac-client "1.3.1-SNAPSHOT"
   :description "Tools for interacting with PE RBAC"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
